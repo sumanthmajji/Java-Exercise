0833db14c013327c18c50a2517a6f866e7c0810b
sumanthmajji <96422695+sumanthmajji@users.noreply.github.com>
2021-12-20 17:23:49 +0530

    Add files via upload

---
+++ exercise.java
@@ -0,0 +1,132 @@
+public class exercise{  
+    public static void main(String args[]){  
+		//Write a Java program to print 'Hello' on screen and then print your name on a separate line
+		System.out.println("S.No-1");  
+		System.out.println("Hello!");  
+		System.out.println("Sumanth"); 
+		System.out.println();	 
+	 
+		//Write a Java program to print the sum of two numbers
+		System.out.println("S.No-2");  
+		System.out.println(74+36);
+		System.out.println();
+		
+		//Write a Java program to divide two numbers and print on the screen. 
+		System.out.println("S.No-3");  
+		System.out.println(50/3);
+		System.out.println();
+		
+		//Write a Java program to print the result of the following operations
+		System.out.println("S.No-4");  
+		System.out.println(-5 + 8 * 6);
+		System.out.println((55+9) % 9);
+		System.out.println(20 + -3*5 / 8);
+		System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+		System.out.println();
+		
+		//Write a Java program that takes two numbers as input and display the product of two numbers. 
+		System.out.println("S.No-5");  
+		int x=25;
+		int y=5;
+		System.out.println(x*y);
+		System.out.println();
+		
+		//Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers
+		System.out.println("S.No-6");  
+		int p=125;
+		int q=24;
+		System.out.print((p+q) + "\n" + (p-q) + "\n" + (p*q) + "\n" + (p/q) + "\n" + (p%q) + "\n");
+		System.out.println();
+		
+		//Write a Java program that takes a number as input and prints its multiplication table upto 10. 
+		System.out.println("S.No-7");  
+		int i=8;
+		System.out.print( "8*1="+(8*1) + "\n" + "8*2="+(8*2) + "\n" + "8*3="+(8*3) + "\n" +"8*4="+(8*4) + "\n" +"8*5="+(8*5) + "\n" +
+							"8*6="+(8*6) + "\n" +"8*7="+(8*7) + "\n" +"8*8="+(8*8) + "\n" +"8*9="+(8*9) + "\n" +"8*10="+(8*10) + "\n");
+		System.out.println();
+		
+		//Write a Java program to display the following pattern
+		System.out.println("S.No-8");  
+		System.out.println("J    a   v     v  a");
+		System.out.println("J   a a   v   v  a a");
+		System.out.println("J  J  aaaaa   V V  aaaaa");
+		System.out.println("JJ  a     a   V  a     a");
+		System.out.println();	 
+		
+		//Write a Java program to compute the specified expressions and print the output. 
+		System.out.println("S.No-9");  
+		System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));  
+		System.out.println();
+		
+		//Write a Java program to compute a specified formula. 
+		System.out.println("S.No-10");  
+		System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));  
+		System.out.println();
+		
+		//Write a Java program to print the area and perimeter of a circle. 
+		System.out.println("S.No-11");  
+		double r=7.5;
+		double pie=3.14159;
+		double perimeter=(2*pie*r);
+		double area=(pie*r*r);
+		System.out.println("Perimeter is = "+(perimeter)+"\n"+"Area is = "+(area));
+		System.out.println();
+		
+		//Write a Java program that takes three numbers as input to calculate and print the average of the numbers
+		System.out.println("S.No-12");  
+		int a=10;
+		int b=20;
+		int c=30;
+		float d=(a+b+c)/3;
+		System.out.println("Average is = "+(d));
+		System.out.println();
+		
+		//Write a Java program to print the area and perimeter of a rectangle. 
+		System.out.println("S.No-13");  
+		double w=5.5;
+		double h=8.5;
+		double per=(2*(w+h));
+		double ar=(w*h);
+		System.out.println("Perimeter is = "+(per)+"\n"+"Area is = "+(ar));
+		System.out.println();
+		
+		//Write a Java program to print an American flag on the screen
+		System.out.println("S.No-14");  
+		System.out.println("* * * * * * ==================================");
+		System.out.println(" * * * * *  ==================================");                          
+		System.out.println("* * * * * * ==================================");
+		System.out.println(" * * * * *  =================================="); 
+		System.out.println("* * * * * * ==================================");
+		System.out.println(" * * * * *  =================================="); 
+		System.out.println("* * * * * * ==================================");
+		System.out.println(" * * * * *  =================================="); 
+		System.out.println("* * * * * * ==================================");
+		System.out.println("* * * * * * ==================================");
+		System.out.println("============================================== ");
+		System.out.println("============================================== ");
+		System.out.println("============================================== ");
+		System.out.println("============================================== ");
+		System.out.println("============================================== ");
+		System.out.println("============================================== ");
+		System.out.println();
+		
+		//Write a Java program to swap two variables
+		System.out.println("S.No-15");
+		int first = 500, second = 1000;
+ 
+        System.out.println("Before Swap");
+        System.out.println("first = " + first);
+        System.out.println("second = " + second);
+ 
+        int temp = first;
+        first = second;
+        second = temp;
+ 
+        System.out.println("After swap");
+        System.out.println("first = " + first);
+        System.out.println("second = " + second);
+		System.out.println();
+    }  
+}  
+
+
